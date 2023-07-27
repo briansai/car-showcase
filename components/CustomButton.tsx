647f4df0b935814fb82dbfa350250588739f852c
Brian Sai <brianwsai@gmail.com>
2023-07-27 10:35:39 -0700

    Created CustomButton and implemented on Hero

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+'use client';
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type="button"
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
 };
 
 export default CustomButton;
