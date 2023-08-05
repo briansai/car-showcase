e8914c2e9648905cd172f7a909b669f8bc924504
Brian Sai <brianwsai@gmail.com>
2023-08-04 19:09:38 -0700

    Initiated Catalogue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Hero } from '@/components';
-import Image from 'next/image';
+import { Catalogue, Hero } from '@/components';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <Catalogue />
     </main>
   );
 }
---
+++ components/Catalogue.tsx
@@ -0,0 +1,19 @@
+const Catalogue = () => {
+  return (
+    <div className="mt-12 padding-x padding-y max-with" id="discover">
+      <div className="home__text-container">
+        <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+        <p>Explore the cars you might like</p>
+      </div>
+      <div className="home__filters">
+        {/*<SearchBar />*/}
+        <div className="home__filter-container">
+          {/*<CustomFilter title="fuel" />*/}
+          {/*<CustomFilter title="year" />*/}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Catalogue;
