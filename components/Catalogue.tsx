2c270a96523a4b0be7ed5576a8b571ce5a0d19a6
Brian Sai <brianwsai@gmail.com>
2023-08-04 19:10:31 -0700

    Fixed spelling of catalog

--- components/Catalogue.tsx
+++ components/Catalogue.tsx
@@ -2,7 +2,7 @@ const Catalogue = () => {
   return (
     <div className="mt-12 padding-x padding-y max-with" id="discover">
       <div className="home__text-container">
-        <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+        <h1 className="text-4xl font-extrabold">Car Catalog</h1>
         <p>Explore the cars you might like</p>
       </div>
       <div className="home__filters">
